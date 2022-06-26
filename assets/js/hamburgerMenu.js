9bf05b181effcdf70fa6990e315550016fe6b290
bax <dev.lasota@gmail.com>
2022-06-26 13:56:20 +0200

    fix bug toggle switch

--- assets/js/hamburgerMenu.js
+++ assets/js/hamburgerMenu.js
@@ -1,14 +1,25 @@
 function hamburgerMenu() {
   let x = document.getElementById("myTopnav");
   let toggle = document.getElementById("toggle");
+
   if (x.className === "topnav") {
     x.className += " responsive";
+
+    if (toggle.classList.contains("active")) {
+      console.log("ma");
+    } else {
+      console.log("nie ma");
+    }
   } else {
     x.className = "topnav";
   }
   if (toggle.className === "toggle") {
     toggle.className += " responsive";
-  } else {
+  } else if (toggle.className == "toggle responsive") {
     toggle.className = "toggle";
+  } else if (toggle.className === "toggle active") {
+    toggle.className += " responsive";
+  } else if (toggle.className === "toggle responsive active") {
+    toggle.className = "toggle active";
   }
 }
